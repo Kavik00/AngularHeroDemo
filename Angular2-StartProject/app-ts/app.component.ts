7e051975734642ddc924652ce7c9cf7ee36c6d51
Gary Griffeth <gary@griffeth.com>
2016-09-27 01:15:30 -0600

    up to multiple Components

--- Angular2-StartProject/app-ts/app.component.ts
+++ Angular2-StartProject/app-ts/app.component.ts
@@ -1,7 +1,36 @@
 ﻿import { Component } from '@angular/core';
 
+export class Hero {
+    id: number;
+    name: string;
+}
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+
 @Component({
     selector: 'my-app',
-    templateUrl: 'app/views/app.component.html'
+    templateUrl: 'app/views/app.component.html',
+    styleUrls:['app/css/app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+    title = 'Tour of Heros';
+    selectedHero: Hero;
+    heroes = HEROES;
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+}
+
+}
